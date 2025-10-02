408e595e2b90f16b2fe3fecfd4c3be2f15cb94f4
fenodevoteam <122632524+fenodevoteam@users.noreply.github.com>
2025-10-02 15:30:00 +0200

    new messages

--- obj/Debug/net9.0/back.AssemblyInfo.cs
+++ obj/Debug/net9.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eec7773554383709962f6afb266cb7ab8b316017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7609b2f5d5c950f9c85dd2b61f793052d8024d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
